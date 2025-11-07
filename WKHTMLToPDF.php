4a7afd676348b08ceba1ac6a8a41358eeedab695
John Wilhite <6500087+johnwilhite@users.noreply.github.com>
2025-11-07 16:08:17 -0500

    Update WKHTMLToPDF.php

--- WKHTMLToPDF.php
+++ WKHTMLToPDF.php
@@ -4,5 +4,5 @@ namespace Rton\WKHTMLToPDF;
 class WKHTMLToPDF
 {
     const PATH = __DIR__ . '/bin/wkhtmltopdf-amd64';
-    const ARMPATH = __DIR__ . '/bin/wkhtmltopdf-arm64';
+    const ARM_PATH = __DIR__ . '/bin/wkhtmltopdf-arm64';
 }
